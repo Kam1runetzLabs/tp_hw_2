445701f3651ba7f7438f69b7dcf63277d376bf3b
Kam1runetz <notsoserious2017@gmail.com>
2021-03-17 15:15:12 +0300

    sources fixed

--- src/mylib/include/header.h
+++ src/mylib/include/header.h
@@ -3,7 +3,6 @@
 #ifndef INCLUDE_INCLUDE_H_
 #define INCLUDE_INCLUDE_H_
 
+void foo();
 
-
-#endif // INCLUDE_INCLUDE_H_
-
+#endif  // INCLUDE_INCLUDE_H_
--- src/mylib/sources/source.c
+++ src/mylib/sources/source.c
@@ -1,4 +1,5 @@
 // Copyright 2021 Kam1runetzLabs <notsoserious2017@gmail.com>
 
-#include "include.h"
+#include "header.h"
 
+void foo() {}
