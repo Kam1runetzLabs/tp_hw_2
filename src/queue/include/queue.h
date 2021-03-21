1a0a3b80e212ef8c364a8c60ea0ff71b1f4dd066
Kam1runetz <notsoserious2017@gmail.com>
2021-03-22 01:01:08 +0300

    queue ready

--- src/queue/include/queue.h
+++ src/queue/include/queue.h
@@ -5,4 +5,20 @@
 #ifndef TEMPLATE_QUEUE_H
 #define TEMPLATE_QUEUE_H
 
+#include <stddef.h>
+
+typedef struct node node_t;
+
+typedef struct queue queue_t;
+
+queue_t *init_queue();
+
+int enqueue(queue_t *queue, void *value);
+
+void *dequeue(queue_t *queue);
+
+size_t queue_size(queue_t *queue);
+
+void free_queue(queue_t *queue);
+
 #endif  // TEMPLATE_QUEUE_H
--- src/queue/sources/queue.c
+++ src/queue/sources/queue.c
@@ -1,4 +1,68 @@
-//
-// Created by w1ckedente on 21.03.2021.
-//
 
+#include "queue.h"
+
+#include <malloc.h>
+#include <stddef.h>
+
+struct node {
+  struct node *next;
+  void *value;
+};
+
+struct queue {
+  node_t *head;
+  node_t *tail;
+  size_t size;
+};
+
+queue_t *init_queue() {
+  queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
+  if (!queue) return NULL;
+  queue->head = queue->tail = NULL;
+  queue->size = 0;
+  return queue;
+}
+
+int enqueue(queue_t *queue, void *value) {
+  if (!queue) return -1;
+
+  node_t *tmp = (node_t *)malloc(sizeof(node_t));
+  if (!tmp) return -1;
+
+  tmp->next = NULL;
+  tmp->value = value;
+
+  if (queue->tail) {
+    queue->tail->next = tmp;
+    queue->tail = tmp;
+  } else {
+    queue->head = queue->tail = tmp;
+  }
+
+  ++queue->size;
+  return 0;
+}
+
+void *dequeue(queue_t *queue) {
+  if (!queue) return NULL;
+  if (!queue->head) return NULL;
+
+  void *ret_value = queue->head->value;
+  node_t *tmp_head = queue->head;
+  queue->head = queue->head->next;
+  free(tmp_head);
+  --queue->size;
+  return ret_value;
+}
+
+size_t queue_size(queue_t *queue) { return queue->size; }
+
+void free_queue(queue_t *queue) {
+  if (!queue) return;
+  if (!queue->head) return;
+  while (queue->head) {
+    node_t *tmp_head = queue->head;
+    queue->head = queue->head->next;
+    free(tmp_head);
+  }
+}
