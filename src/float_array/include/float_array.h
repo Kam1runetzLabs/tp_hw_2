408a33eaeabace9ff1d8e6af424c772877e22bc7
Kam1runetz <notsoserious2017@gmail.com>
2021-03-23 00:06:38 +0300

    float array ready

--- src/float_array/include/float_array.h
+++ src/float_array/include/float_array.h
@@ -5,4 +5,26 @@
 #ifndef TEMPLATE_FLOAT_ARRAY_H
 #define TEMPLATE_FLOAT_ARRAY_H
 
+#include <stddef.h>
+
+typedef struct float_array float_array_t;
+typedef float *iterator;
+typedef const float *const_iterator;
+
+float_array_t *init_float_array(size_t size);
+
+void free_float_array(float_array_t *array);
+
+size_t float_array_size(const float_array_t *array);
+
+iterator float_array_begin(const float_array_t *array);
+
+iterator float_array_end(const float_array_t *array);
+
+const_iterator float_array_cbegin(const float_array_t *array);
+
+const_iterator float_array_cend(const float_array_t *array);
+
+float get_element(float_array_t *array, size_t index);
+
 #endif  // TEMPLATE_FLOAT_ARRAY_H
--- src/float_array/sources/float_array.c
+++ src/float_array/sources/float_array.c
@@ -1,3 +1,62 @@
 //
 // Created by w1ckedente on 22.03.2021.
 //
+#include "float_array.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+struct float_array {
+  float* memory;
+  size_t size;
+};
+
+float_array_t* init_float_array(size_t size) {
+  assert(size != 0);
+  float_array_t* array = (float_array_t*)malloc(sizeof(float_array_t));
+  if (!array) return NULL;
+
+  array->memory = (float*)calloc(size, sizeof(float));
+  if (!array->memory) {
+    free(array);
+    return NULL;
+  }
+  array->size = size;
+  return array;
+}
+
+void free_float_array(float_array_t* array) {
+  if (!array) return;
+  free(array->memory);
+  free(array);
+}
+
+size_t float_array_size(const float_array_t* array) {
+  assert(array != NULL);
+  return array->size;
+}
+
+iterator float_array_begin(const float_array_t* array) {
+  assert(array != NULL);
+  return array->memory;
+}
+
+iterator float_array_end(const float_array_t* array) {
+  assert(array != NULL);
+  return array->memory + array->size;
+}
+
+const_iterator float_array_cbegin(const float_array_t* array) {
+  return float_array_begin(array);
+}
+
+const_iterator float_array_cend(const float_array_t* array) {
+  return float_array_end(array);
+}
+
+float get_element(float_array_t* array, size_t index) {
+  assert(array != NULL);
+  assert(index < array->size);
+  return array->memory[index];
+}
