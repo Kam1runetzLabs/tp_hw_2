1484ff0fcb5b52193feabda89af73842ad1f920f
Kam1runetz <notsoserious2017@gmail.com>
2021-03-23 01:07:52 +0300

    utility.c fix

--- src/3d_vectors/sources/utility.c
+++ src/3d_vectors/sources/utility.c
@@ -18,7 +18,7 @@ size_t fill_vectors(FILE *file, vectors_3d_t *vectors) {
 
   size_t capacity = vectors_capacity(vectors);
   for (; read_count != capacity && !feof(file); ++read_count) {
-    if (fscanf(file, "%f%f%f", &x, &y, &z) != 3) return read_count;
+    if (fscanf(file, "%f%f%f", &x, &y, &z) != 3) return read_count - 1;
     add_vector(vectors, x, y, z);
   }
 
