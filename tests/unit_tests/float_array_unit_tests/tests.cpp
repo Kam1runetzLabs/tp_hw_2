e115b9f879ed54903ba222040ccf7774636dbdee
Kam1runetz <notsoserious2017@gmail.com>
2021-03-23 01:27:10 +0300

    tests sceleton created

---
+++ tests/unit_tests/float_array_unit_tests/tests.cpp
@@ -0,0 +1,9 @@
+// Copyright 2021 Kam1runetzLabs <notsoserious2017@gmail.com>
+
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "float_array.h"
+}
+
+TEST(Example, EmtpyTest) { EXPECT_TRUE(true); }
