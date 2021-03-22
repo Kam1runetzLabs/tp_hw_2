3d7ed6a9fccdc7fe6104be875e821fe8b3122105
Kam1runetz <notsoserious2017@gmail.com>
2021-03-23 00:55:57 +0300

    start thread_pool working

--- src/thread_pool/include/thread_pool.h
+++ src/thread_pool/include/thread_pool.h
@@ -5,4 +5,20 @@
 #ifndef TEMPLATE_THREAD_POOL_H
 #define TEMPLATE_THREAD_POOL_H
 
-#endif //TEMPLATE_THREAD_POOL_H
+#include <stddef.h>
+
+typedef struct thread_pool thread_pool_t;
+
+typedef void *(*task_t)(void *);
+typedef void (*callback_t)(void *);
+
+thread_pool_t *init_thread_pool(size_t workers_count);
+
+int enqueue_task(thread_pool_t *tp, task_t task, void *args,
+                 callback_t callback);
+
+void wait_thread_pool(thread_pool_t *tp);
+
+void destroy_thread_pool(thread_pool_t *tp);
+
+#endif  // TEMPLATE_THREAD_POOL_H
--- src/thread_pool/sources/thread_pool.c
+++ src/thread_pool/sources/thread_pool.c
@@ -1,3 +1,10 @@
 //
 // Created by w1ckedente on 21.03.2021.
 //
+
+#include <pthread.h>
+#include <stdlib.h>
+
+#include "thread_pool.h"
+#include "queue.h"
+
