99c4e989215d4ad9fd622e24614e72bc2138aac0
Kam1runetz <notsoserious2017@gmail.com>
2021-03-29 22:06:33 +0300

    checks script created

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -6,7 +6,6 @@ export CTEST_OUTPUT_ON_FAILURE=true
 export ASAN_OPTIONS=allocator_may_return_null=1
 export TSAN_OPTIONS=allocator_may_return_null=1
 
-
 # address sanitizer
 CMAKE_CONFIG_OPTIONS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
 CMAKE_TOOLCHAIN_OPTIONOS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/sanitize-address.cmake'"
@@ -14,8 +13,12 @@ CMAKE_OPTIONS="$CMAKE_CONFIG_OPTIONS $CMAKE_TOOLCHAIN_OPTIONS"
 
 cmake -H. -B_builds/sanitize_address $CMAKE_OPTIONS
 cmake --build _builds/sanitize_address
-./_builds/sanitize_address/tests/unit_tests/unit_tests
-
+./_builds/sanitize_address/tests/unit_tests/float_array_unit_tests/float_array_unit_tests
+./_builds/sanitize_address/tests/unit_tests/multithreading_vectors_calculating_unit_tests/m_v_calculating_unit_tests
+./_builds/sanitize_address/tests/unit_tests/queue_unit_tests/queue_unit_tests
+./_builds/sanitize_address/tests/unit_tests/sync_vectors_calculating_unit_tests/s_v_calculating_unit_tests
+./_builds/sanitize_address/tests/unit_tests/thread_pool_unit_tests/thread_pool_unit_tests
+./_builds/sanitize_address/tests/unit_tests/vectors_unit_tests/vectors_unit_tests
 
 # thread sanitizer
 CMAKE_CONFIG_OPTIONS="-DHUNTER_CONFIGURATION_TYPE=Debug -DCMAKE_BUILD_TYPE=Debug"
@@ -24,8 +27,12 @@ CMAKE_OPTIONS="$CMAKE_TOOLCHAIN_OPTIONS $CMAKE_CONFIG_OPTIONS"
 
 cmake -H. -B_builds/sanitize_thread $CMAKE_OPTIONS
 cmake --build _builds/sanitize_thread
-./_builds/sanitize_thread/tests/unit_tests/unit_tests
-
+./_builds/sanitize_thread/tests/unit_tests/float_array_unit_tests/float_array_unit_tests
+./_builds/sanitize_thread/tests/unit_tests/multithreading_vectors_calculating_unit_tests/m_v_calculating_unit_tests
+./_builds/sanitize_thread/tests/unit_tests/queue_unit_tests/queue_unit_tests
+./_builds/sanitize_thread/tests/unit_tests/sync_vectors_calculating_unit_tests/s_v_calculating_unit_tests
+./_builds/sanitize_thread/tests/unit_tests/thread_pool_unit_tests/thread_pool_unit_tests
+./_builds/sanitize_thread/tests/unit_tests/vectors_unit_tests/vectors_unit_tests
 
 # valgrind memcheck
 CMAKE_CONFIG_OPTIONS="-DHUNTER_CONFIGURATION_TYPE=Debug -DCMAKE_BUILD_TYPE=Debug"
@@ -33,5 +40,9 @@ CMAKE_OPTIONS="$CMAKE_CONFIG_OPTIONS"
 
 cmake -H. -B_builds/memcheck $CMAKE_OPTIONS
 cmake --build _builds/memcheck
-valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/unit_tests
-
+valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/float_array_unit_tests/float_array_unit_tests
+valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/multithreading_vectors_calculating_unit_tests/m_v_calculating_unit_tests
+valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/queue_unit_tests/queue_unit_tests
+valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/sync_vectors_calculating_unit_tests/s_v_calculating_unit_tests
+valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/thread_pool_unit_tests/thread_pool_unit_tests
+valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/vectors_unit_tests/vectors_unit_tests
