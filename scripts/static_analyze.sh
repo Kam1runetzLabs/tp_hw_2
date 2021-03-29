e98addedbf84d06036c4c2b55ecb71613877f2ed
Kam1runetz <notsoserious2017@gmail.com>
2021-03-29 22:11:53 +0300

    static analyze script fixed

--- scripts/static_analyze.sh
+++ scripts/static_analyze.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CXX_CPPCHECK_OPTIONS="--language=c --std=c11 --verbose "
-C_CPPCHECK_OPTIONS="--language=c++ --std=c++11 --verbose "
+C_CPPCHECK_OPTIONS="--language=c --std=c11 --verbose "
+CXX_CPPCHECK_OPTIONS="--language=c++ --std=c++11 --verbose "
 
 cppcheck $C_CPPCHECK_OPTIONS --xml --xml-version=2 ./src 2>stat-analyze-report-src.xml
 cppcheck-htmlreport --file=stat-analyze-report-src.xml --report-dir=stat-analyze-report-src
