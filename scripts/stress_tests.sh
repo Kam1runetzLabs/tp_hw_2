203e1fc13e5ca149f3fb437fda04380b35a892c2
Kam1runetz <notsoserious2017@gmail.com>
2021-03-31 13:14:27 +0300

    stress tests fixed

--- scripts/stress_tests.sh
+++ scripts/stress_tests.sh
@@ -9,14 +9,19 @@ export VECTORS_PATH="vectors.txt"
 export VECTORS_COUNT=33000000
 export VECTORS_DIMS=3
 
+echo "Generating vectors file..."
 python "$(pwd)/scripts/generator.py"
 
-cmake -H. -B_builds/sync -DMTHREADING_CALC=OFF "$CMAKE_OPTS"
-cmake --build _builds/sync --target stress_tests
-cmake -H. -B_builds/mthreading -DMTHREADING_CALC=ON "$CMAKE_OPTS"
-cmake --build _builds/mthreading --target stress_tests
+cmake -H. -B_builds -DMTHREADING_CALC=ON "$CMAKE_OPTS"
+cmake --build _builds --target stress_tests
+mthreading="$(./_builds/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH)"
+
+rm -rf _builds
+cmake -H. -B_builds -DMTHREADING_CALC=OFF "$CMAKE_OPTS"
+cmake --build _builds --target stress_tests
+not_mthreading="$(./_builds/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH)"
 
 echo "Not use multithreading: "
-./_builds/sync/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH
+echo "$not_mthreading"
 echo "Use multithreading: "
-./_builds/mthreading/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH
+echo "$mthreading"
