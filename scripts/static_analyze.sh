7a8c30adb4c0ad8225e04fed97a5118dcf78efb1
Kam1runetz <notsoserious2017@gmail.com>
2021-03-31 18:58:21 +0300

    trying codecov

--- scripts/static_analyze.sh
+++ scripts/static_analyze.sh
@@ -4,12 +4,4 @@ C_CPPCHECK_OPTIONS="--language=c --std=c11 --verbose "
 CXX_CPPCHECK_OPTIONS="--language=c++ --std=c++11 --verbose "
 
 cppcheck $C_CPPCHECK_OPTIONS --xml --xml-version=2 ./src 2>stat-analyze-report-src.xml
-cppcheck-htmlreport --file=stat-analyze-report-src.xml --report-dir=stat-analyze-report-src
-
 cppcheck $CXX_CPPCHECK_OPTIONS --xml --xml-version=2 ./tests 2>stat-analyze-report-tests.xml
-cppcheck-htmlreport --file=stat-analyze-report-tests.xml --report-dir=stat-analyze-report-tests
-
-mkdir static-analyze-report/
-rm *.xml
-mv stat-analyze-report* static-analyze-report/
-rm -rf stat-analyze*
