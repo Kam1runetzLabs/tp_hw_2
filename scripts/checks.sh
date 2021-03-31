b9c06854940d06575a89f10e70645fd2c6aa4639
Kam1runetz <notsoserious2017@gmail.com>
2021-03-31 14:05:36 +0300

    script cleanup

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -11,7 +11,7 @@ CMAKE_CONFIG_OPTIONS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debu
 CMAKE_TOOLCHAIN_OPTIONOS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/sanitize-address.cmake'"
 CMAKE_OPTIONS="$CMAKE_CONFIG_OPTIONS $CMAKE_TOOLCHAIN_OPTIONS"
 
-cmake -H. -B_builds/sanitize_address $CMAKE_OPTIONS
+cmake -H. -B_builds/sanitize_address "$CMAKE_OPTIONS"
 cmake --build _builds/sanitize_address
 ./_builds/sanitize_address/tests/unit_tests/float_array_unit_tests/float_array_unit_tests
 ./_builds/sanitize_address/tests/unit_tests/multithreading_vectors_calculating_unit_tests/m_v_calculating_unit_tests
@@ -25,7 +25,7 @@ CMAKE_CONFIG_OPTIONS="-DHUNTER_CONFIGURATION_TYPE=Debug -DCMAKE_BUILD_TYPE=Debug
 CMAKE_TOOLCHAIN_OPTIONS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/sanitize-thread.cmake'"
 CMAKE_OPTIONS="$CMAKE_TOOLCHAIN_OPTIONS $CMAKE_CONFIG_OPTIONS"
 
-cmake -H. -B_builds/sanitize_thread $CMAKE_OPTIONS
+cmake -H. -B_builds/sanitize_thread "$CMAKE_OPTIONS"
 cmake --build _builds/sanitize_thread
 ./_builds/sanitize_thread/tests/unit_tests/float_array_unit_tests/float_array_unit_tests
 ./_builds/sanitize_thread/tests/unit_tests/multithreading_vectors_calculating_unit_tests/m_v_calculating_unit_tests
@@ -38,7 +38,7 @@ cmake --build _builds/sanitize_thread
 CMAKE_CONFIG_OPTIONS="-DHUNTER_CONFIGURATION_TYPE=Debug -DCMAKE_BUILD_TYPE=Debug"
 CMAKE_OPTIONS="$CMAKE_CONFIG_OPTIONS"
 
-cmake -H. -B_builds/memcheck $CMAKE_OPTIONS
+cmake -H. -B_builds/memcheck "$CMAKE_OPTIONS"
 cmake --build _builds/memcheck
 valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/float_array_unit_tests/float_array_unit_tests
 valgrind --tool=memcheck --leak-check=full --track-origins=yes ./_builds/memcheck/tests/unit_tests/multithreading_vectors_calculating_unit_tests/m_v_calculating_unit_tests
