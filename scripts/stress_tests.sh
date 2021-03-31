2f5dc1754b573ede471725ae707f7cc269894b8a
Kam1runetz <notsoserious2017@gmail.com>
2021-03-31 12:10:59 +0300

    stress tests work ready

--- scripts/stress_tests.sh
+++ scripts/stress_tests.sh
@@ -6,18 +6,17 @@ CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/gcc-pic.cmake'"
 CMAKE_OPTS="$CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 
 export VECTORS_PATH="vectors.txt"
-export VECTORS_COUNT=33
+export VECTORS_COUNT=33000000
 export VECTORS_DIMS=3
 
 python "$(pwd)/scripts/generator.py"
 
-cmake -H. -B_builds -DMTHREADING_CALC=OFF "$CMAKE_OPTS"
-mv vectors.txt _builds/tests/stress_tests
-cmake --build _builds
-echo "Not use multithreading: "
-./_builds/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH
+cmake -H. -B_builds/sync -DMTHREADING_CALC=OFF "$CMAKE_OPTS"
+cmake --build _builds/sync
+cmake -H. -B_builds/mthreading -DMTHREADING_CALC=ON "$CMAKE_OPTS"
+cmake --build _builds/mthreading
 
-cmake -H. -B_builds -DMTHREADING_CALC=ON "$CMAKE_OPTS"
-cmake --build _builds
+echo "Not use multithreading: "
+./_builds/sync/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH
 echo "Use multithreading: "
-./_builds/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH
+./_builds/mthreading/tests/stress_tests/stress_tests $VECTORS_COUNT $VECTORS_DIMS $VECTORS_PATH
--- tests/stress_tests/stress_test.cpp
+++ tests/stress_tests/stress_test.cpp
@@ -2,7 +2,9 @@
 
 #include <chrono>
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 
 extern "C" {
 #include "float_array.h"
@@ -11,50 +13,71 @@ extern "C" {
 #include "vectors_calc.h"
 }
 
-// todo switch fprintf to cout
-// todo arsg handiling
-
 int main(int argc, char *argv[]) {
-  const size_t capacity = 123;
-  const size_t dims = 3;
-  std::string f_name;
+  if (argc != 4) {
+    std::cout << "Usage: <program> <capacity> <dims> <vectors/file/path>"
+              << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  std::stringstream convert_capacity(argv[1]);
+  std::stringstream convert_dims(argv[2]);
+
+  std::size_t capacity;
+  std::size_t dims;
+
+  if (!(convert_capacity >> capacity) || !(convert_dims >> dims)) {
+    std::cout << "Invalid arguments types" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  std::string f_name(argv[3]);
 
   vectors_t *vectors = vectors_init(capacity, dims);
   if (!vectors) {
-    fprintf(stderr, "ERROR: Fail of vectors allocation\n");
+    std::cout << "ERROR: Fail of vectors allocation" << std::endl;
     exit(EXIT_FAILURE);
   }
 
   FILE *file = fopen(f_name.c_str(), "r");
   if (!file) {
     vectors_free(vectors);
-    fprintf(stderr, "ERROR: Unable to open file %s\n", f_name.c_str());
+    std::cout << "ERROR: Unable to open file " << f_name << std::endl;
     exit(EXIT_FAILURE);
   }
 
   size_t read_count = fill_vectors(file, vectors);
   if (read_count != capacity) {
-    fprintf(stderr,
-            "WARNING: read count mismatch with vectors capacity\nVectors "
-            "capacity: %lu, read_count: %lu\n",
-            capacity, read_count);
+    std::cout << "WARNING: read count mismatch with vectors capacity"
+              << std::endl
+              << "Vectors capacity: " << capacity
+              << " read_count: " << read_count << std::endl;
   }
+  fclose(file);
 
   auto start = std::chrono::high_resolution_clock::now();
   float_array_t *avg_vector = calc_avg_vector(vectors);
   auto stop = std::chrono::high_resolution_clock::now();
 
   if (!avg_vector) {
-    fprintf(stderr, "ERROR: Fail to calculate average vector\n");
+    std::cout << "ERROR: Fail to calculate average vector" << std::endl;
     vectors_free(vectors);
     exit(EXIT_FAILURE);
   }
 
   auto duration =
       std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+  std::cout << std::left << "| duration: " << duration.count() << " ms |"
+            << "avg_vector: ";
+
+  const std::size_t width = 6;
+  std::cout.precision(2);
+  for (const_iterator it = float_array_cbegin(avg_vector);
+       it != float_array_cend(avg_vector); ++it) {
+    std::cout << std::left << std::setw(width) << *it << ";";
+  }
+  std::cout << std::endl;
   vectors_free(vectors);
   float_array_free(avg_vector);
-
-  std::cout << "duration: " << duration.count() << "ms" << std::endl;
   return 0;
 }
